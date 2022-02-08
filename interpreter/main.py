45906f175cb5a975a6dcd93eca8c05362d70e3d7
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-02-08 21:43:28 +0100

    Add support for running multiple files

--- interpreter/main.py
+++ interpreter/main.py
@@ -31,7 +31,7 @@ class Lexer:
 				else:
 					self.column += len(s)
 				return (s, self.line, self.column)
-		raise SyntaxError("Syntax error at %d:%d in %s" % (self.line, self.column, self.file))
+		raise_error(("Syntax error at %d:%d in %s" % (self.line, self.column, self.file)), 1)
 
 class Validator:
 	def run(self, lex: Lexer):
@@ -41,7 +41,7 @@ class Validator:
 			p = t
 			t = lex.next()
 		if not re.match(":\r?\n?", p[0]):
-			raise SyntaxError("Syntax error at %d:%d" % (lex.line, lex.column))
+			raise_error(("Syntax error at %d:%d in %s - ':' expected" % (lex.line, lex.column, lex.file)), 1)
 
 class Parser:
 	def __init__(self):
@@ -79,20 +79,44 @@ class AUI:
 			p = t
 			t = lex.next()
 		if p[0] != ":" and p[0] != ":\n" and p[0] != ":\r\n":
-			raise SyntaxError("Syntax error at %d:%d" % (lex.line, lex.column))
+			raise_error(("Syntax error at %d:%d in %s - ':' expected" % (lex.line, lex.column, lex.file)), 1)
 
 class Runner:
 	def __init__(self, root_path):
 		self.root_path = root_path
+		self.valid_commands = [
+			"\\[@",
+			"@\\]",
+			"\\[",
+			"\\]",
+			"\\$.",
+			"\\\\.",
+			"!",
+			":\r?\n?",
+			"\".*\"",
+			"[ \t\f\v]",
+			":$",
+			"\$",
+			"\^"
+		]
 
 	def run_file(self, file_path):
-		pass
+		file = open(os.path.join(path, file_path), "r")
+		program = file.read()
+		file.close()
+		self.run(program, file_path)
 
-	def run_user_input(self, input):
-		self.run()
+	def run_user_input(self, program):
+		self.run(program, "<input_main>")
 
-	def run(self, input):
-		pass
+	def run(self, program, file):
+		print(repr(program))
+		lexer = Lexer(program, self.valid_commands, file)
+		validator = Validator()
+		validator.run(deepcopy(lexer))
+		parser = Parser()
+		parser.run(deepcopy(lexer))
+		commands = parser.commands
 
 class Warner:
 	def __init__(self, flags):
@@ -111,6 +135,10 @@ class Warner:
 			print("Warning: No code path supplied, this will make it impossible to run files from the code (you can use the --disable-warnings flag to disable all warnings or --disable-path-warning to disable this particular warning")
 			return
 
+def raise_error(text, code = 1):
+		print(text)
+		sys.exit(code)
+
 if __name__ == "__main__":
 	args = sys.argv
 	debug_heavy = False
@@ -123,6 +151,7 @@ if __name__ == "__main__":
 	]
 	args_amount = len(args)
 	path = None
+	program = None
 	for i in range(0, args_amount):
 		arg = args[i]
 		if arg == "-" and not "-" in flags and i < args_amount - 1:
@@ -135,36 +164,13 @@ if __name__ == "__main__":
 		path: str = input("Input the complete path to your maumivu.au file: ") if args_amount < 2 else args[1]
 		if path.endswith("maumivu.au"):
 			path = path[0:-10]
-		file = open(os.path.join(path, "maumivu.au"), "r")
-		program = file.read()
-		file.close()
 
 	warner = Warner(flags)
 	if path == None:
 		warner.warn("path")
 
-	print("Program:")
-	print(repr(program))
-
-	valid_commands = [
-		"\\[@",
-		"@\\]",
-		"\\[",
-		"\\]",
-		"\\$.",
-		"\\\\.",
-		"!",
-		":\r?\n?",
-		"\".*\"",
-		"[ \t\f\v]",
-		":$",
-		"\$",
-		"\^"
-	]
-	lexer = Lexer(program, valid_commands, "maumivu.au")
-	validator = Validator()
-	validator.run(deepcopy(lexer))
-	parser = Parser()
-	parser.run(deepcopy(lexer))
-	i = AUI()
-	i.run(deepcopy(lexer))
+	runner = Runner(path)
+	if program == None:
+		runner.run_file("maumivu.au")
+	else:
+		runner.run_user_input(program)
