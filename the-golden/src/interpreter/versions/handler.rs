1349006f0457daf1bf3604bad39d4ee549a92395
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-08-06 12:04:38 +0200

    Add version 0.2.0 with the ?? command

--- the-golden/src/interpreter/versions/handler.rs
+++ the-golden/src/interpreter/versions/handler.rs
@@ -1,14 +1,19 @@
 use crate::Flags;
 
-#[path = "./v0-1-0/main.rs"] mod v0_1_0;
+#[path = "./v0-1-0/main.rs"]
+mod v0_1_0;
+#[path = "./v0-2-0/main.rs"]
+mod v0_2_0;
 
 pub struct Handler {
-	versions: [String; 1]
+	versions: [String; 2],
 }
 
 impl Handler {
 	pub fn new() -> Self {
-		Self { versions: [String::from("0.1.0")] }
+		Self {
+			versions: [String::from("0.1.0"), String::from("0.2.0")],
+		}
 	}
 
 	pub fn parse_version(&self, mut version: String) -> String {
@@ -22,7 +27,8 @@ impl Handler {
 	pub fn run(&self, version: String, code: String, code_path: std::path::PathBuf, flags: Flags) {
 		match version.as_str() {
 			"0.1.0" => v0_1_0::Runner::new(code, code_path, flags).run(),
-			_ => panic!("Couldn't launch version {}", &version)
+			"0.2.0" => v0_2_0::Runner::new(code, code_path, flags).run(),
+			_ => panic!("Couldn't launch version {}", &version),
 		}
 	}
 }
